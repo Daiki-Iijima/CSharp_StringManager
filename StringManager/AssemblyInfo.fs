19a88eeddb1ac6edd3787145dc25dc4fb849a13e
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-08-04 10:40:36 +0900

    プロジェクト追加

---
+++ StringManager/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace StringManager
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("StringManager")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
